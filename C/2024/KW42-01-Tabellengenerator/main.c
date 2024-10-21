11cf018e4790bdf1f22d6e0182a99bedeff9c4ac
Nepomuk Crhonek <nepomuk@crhonek.com>
2024-10-21 12:05:06 +0200

    custom border hinzugefügt

--- C/2024/KW42-01-Tabellengenerator/main.c
+++ C/2024/KW42-01-Tabellengenerator/main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(void) {
     int zeilen;
     int spalten;
+    int pixel;
+    
+    printf("Wie viele Spalten?\n");
+    scanf("%d", &spalten);
 
     printf("Wie viele Zeilen?\n");
     scanf("%d", &zeilen);
-    printf("Wie viele Spalten?\n");
-    scanf("%d", &spalten);
+
+    printf("Border in pixel: ");
+    scanf("%d", &pixel);
+
 
     FILE *index;
     index = fopen("../index.html", "w");
@@ -20,6 +26,11 @@ int main(void) {
     fprintf(index, "<!DOCTYPE html>\n");
     fprintf(index, "<html lang=\"de\">\n");
     fprintf(index, "  <head>\n");
+    fprintf(index, "      <style>\n");
+    fprintf(index, "          table, th, td {\n");
+    fprintf(index, "              border: %dpx solid;\n", pixel);
+    fprintf(index, "          }\n");
+    fprintf(index, "      </style>\n");
     fprintf(index, "    <meta charset=\"utf-8\">\n");
     fprintf(index, "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n");
     fprintf(index, "    <title>Tabelle</title>\n");
@@ -28,11 +39,23 @@ int main(void) {
     fprintf(index, "    \n");
     fprintf(index, "    <h1>einfache Tabelle von C</h1>\n");
     fprintf(index, "    <table>\n");
-    fprintf(index, "        <tr>\n");
-    for (int i = 0; i < spalten; i++) {
-        fprintf(index, "            <th>Header %d</th>\n", i+1);
+
+    if (zeilen != 0) {
+        fprintf(index, "        <tr>\n");
+        for (int i = 0; i < spalten; i++) {
+            fprintf(index, "            <th>Header %d</th>\n", i+1);
+        }
+        fprintf(index, "        </tr>\n");
+
+        for (int i = 0; i < zeilen-1; i++) {
+            fprintf(index, "        <tr>\n");
+            for (int j = 0; j < spalten; j++) {
+                fprintf(index, "            <th>Hello World</th>\n");
+            }
+            fprintf(index, "        </tr>\n");
+        }
     }
-    fprintf(index, "        </tr>\n");
+
     fprintf(index, "    </table>\n");
     fprintf(index, "  </body>\n");
     fprintf(index, "</html>\n");
