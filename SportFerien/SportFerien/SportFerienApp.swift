4946147d084e67ca50796305c4b99f07c79dacc2
Nepomuk Crhonek <nepomuk@crhonek.com>
2025-02-03 08:59:43 +0100

    SportFerien app creted

---
+++ SportFerien/SportFerien/SportFerienApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportFerienApp.swift
+//  SportFerien
+//
+//  Created by Nepomuk Crhonek on 03.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SportFerienApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
