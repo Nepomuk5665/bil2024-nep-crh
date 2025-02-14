e45bfa35d4125e1576f5499d844c623ec2b97643
Nepomuk Crhonek <105591323+Nepomuk5665@users.noreply.github.com>
2025-02-14 10:50:55 +0100

    New app NepoTerm

---
+++ NepoTerm/NepoTerm/NepoTermApp.swift
@@ -0,0 +1,17 @@
+//
+//  NepoTermApp.swift
+//  NepoTerm
+//
+//  Created by Nepomuk Crhonek on 14.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct NepoTermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
