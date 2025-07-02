261fccf44491ff92e23425bc754ee71dd7624fb0
Nepomuk Crhonek <105591323+Nepomuk5665@users.noreply.github.com>
2025-07-02 11:35:26 +0200

    Initialize ZORK text adventure game project
    - Add basic game structure with main loop
    - Set up project configuration

---
+++ Java/2025/KW27-ZORK/src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Boolean gameOn = true;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello and welcome to ZORK!");
+
+        while (gameOn) {
+            String input = "";
+            System.out.print("> ");
+            input = scanner.nextLine();
+
+
+        }
+    }
+}
